306dd70cfa95a17849a5d809b8b2dbbabe0681a6
Igor Sotarev <igor@sotarev.ru>
2022-05-01 21:07:39 +0500

    Add db classes, update db

--- flask-app/project/models.py
+++ flask-app/project/models.py
@@ -6,4 +6,21 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    role = db.Column(db.String(6))
+
     name = db.Column(db.String(100))
+    position = db.Column(db.String(100))
+    salary_multiplier = db.Column(db.Float)
+
+
+class AbsenceEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    person = db.Column(db.String(100))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    absence_type_number = db.Column(db.String(2))
+    absence_type_text = db.Column(db.String(2))
+
+
+class DayOff(db.Model):
+    day = db.Column(db.DateTime, primary_key=True)
